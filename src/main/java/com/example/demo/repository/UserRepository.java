a1caf5999f399f92dd290c0a01447cfb852ad147
park suhyun <laztwo@hanmail.net>
2018-10-11 00:02:30 +0900

    JPAController

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUserName(@Param("userName") String userName);
+}
