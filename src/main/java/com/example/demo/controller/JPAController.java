1c8d0cbcc72ce7b2f74ebf524f3f2942be6e8a69
park suhyun <laztwo@hanmail.net>
2018-10-24 19:33:28 +0900

    JPA ~ MariaDB

--- src/main/java/com/example/demo/controller/JPAController.java
+++ src/main/java/com/example/demo/controller/JPAController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -30,6 +32,13 @@ public class JPAController {
     @RequestMapping(value = "/findByName", method = RequestMethod.GET)
     public ResponseEntity<UserEntity> findByName(@RequestParam(value = "name") String name) {
         UserEntity user = userRepository.findByName(name);
+
+        return new ResponseEntity(user, OK);
+    }
+
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    public ResponseEntity<List<UserEntity>> findAll() {
+        List<UserEntity> user = userRepository.findAll();
 
         return new ResponseEntity(user, OK);
     }
--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,16 +1,25 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
+/*
+create table user_table (
+    no bigint not null auto_increment,
+    name varchar(10) not null,
+    primary key(no)
+);
+ */
+
 @Entity // hibernate-core
+@Table(name = "user_table")
 public class UserEntity implements Serializable {
     @Id // javax.persistence
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no", nullable = false)
+    private Long no;
+
+    @Column(name = "name", nullable = false)
     private String name;
 
     public UserEntity() {
