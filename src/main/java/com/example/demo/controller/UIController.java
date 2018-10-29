7b99cd58a655f214e6d1191ac6e1955be46104b7
박수현 <suhpark@ebay.com>
2018-10-29 18:36:37 +0900

    RestTemplate

--- src/main/java/com/example/demo/controller/UIController.java
+++ src/main/java/com/example/demo/controller/UIController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.SchoolM;
 import com.example.demo.service.InMemoryProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,5 +39,20 @@ public class UIController {
     public String templatePage3(Model model) {
         model.addAttribute("products", inMemoryProductService.getProductList());
         return "th3";
+    }
+
+    @RequestMapping(value = "/th4")
+    public String templatePage4(Model model) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/jpa/findShcoolInfo", Object[].class);
+        Object[] objects = responseEntity.getBody();
+
+        System.out.println(objects);
+        System.out.println(objects[0]); // SchoolM
+
+        // model.addAttribute("products", inMemoryProductService.getProductList());
+        model.addAttribute("schoolInfo", objects);
+        return "th4";
     }
 }
