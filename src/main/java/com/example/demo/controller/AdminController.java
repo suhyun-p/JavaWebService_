739f5b7d6efff9af116b18b15feb581911869d50
박수현 <suhpark@ebay.com>
2018-10-31 10:40:09 +0900

    StudentInfo

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping(value = "/admin")
@@ -19,5 +21,23 @@ public class AdminController {
         model.addAttribute("message", "boot template");
 
         return "admin/checkout";
+    }
+
+    @RequestMapping(value = "/school")
+    public String School(Model model) {
+        model.addAttribute("message", "boot template");
+
+        return "admin/school";
+    }
+
+    @RequestMapping(value = "/student")
+    public String Student(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/jpa/findStudentInfo", Object[].class);
+        Object[] objects = responseEntity.getBody(); // StudentM
+
+        model.addAttribute("studentlInfo", objects);
+
+        return "admin/student";
     }
 }
