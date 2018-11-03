50759218664b9995ad6194276a33e2fa86f2ff6d
suhpark <laztwo@hanmail.net>
2018-11-03 16:39:44 +0900

    수

--- src/main/java/com/example/demo/service/ClassService.java
+++ src/main/java/com/example/demo/service/ClassService.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.converter.ClassConverter;
 import com.example.demo.entity.ClassT;
 import com.example.demo.model.ClassM;
 import com.example.demo.repository.ClassRepository;
