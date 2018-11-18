3a6cbe078da5a412d56f742c8d14932524b6608c
suhpark <laztwo@hanmail.net>
2018-11-18 18:51:58 +0900

    not modal use data-toggle

--- target/classes/assets/js/user.js
+++ target/classes/assets/js/user.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         columnDefs : [
             {
                 "data": null,
-                "defaultContent": "<a href=\"#\"><i class=\"align-middle mr-2 fas fa-fw fa-edit\"></i></a>",
+                "defaultContent": "<a href=\"#\"><i class=\"align-middle mr-2 fas fa-fw fa-edit\"></i><i class=\"align-middle mr-2 fas fa-fw fa-book-open\"></i></a>",
                 "targets": -1,
                 "orderable": false,
                 "className": 'table-action'
@@ -47,7 +47,7 @@ $(document).ready(function () {
 });
 
 var setBanner = function ( data, type, row ) {
-    data = "<span onclick='showCareer(" + row.no + ")'>" + data + "</span>";
+    data = "<span data-toggle=\"modal\" data-target=\"#defaultModalPrimary\" onclick='loadCareer(" + row.no + ")'>" + data + "</span>";
 
     if(row.sex == "Male") data += ' <span class=\"badge badge-primary\">Male</span>';
     else data += ' <span class=\"badge badge-primary\">Female</span>';
@@ -80,7 +80,7 @@ function RegUser(userNickname, userSex, isInstructor) {
     })
 }
 
-function showCareer(userNo){
+function loadCareer(userNo){
 
     var data = {};
     data["userNo"] = userNo;
@@ -92,7 +92,6 @@ function showCareer(userNo){
         url: '/api/careers/getCareer',
         type: 'POST',
         success: function (response) {
-            var a = 1;
             setCareerInfo(response.data);
         },
         error: function (request, status, error) {
@@ -100,8 +99,8 @@ function showCareer(userNo){
         }
     })
 
-    $("#careerTitle").text(userNo + '\'s Career');
-    $("#defaultModalPrimary").modal();
+    // $("#careerTitle").text(userNo + '\'s Career');
+    // $("#defaultModalPrimary").modal();
 }
 
 function clearAddUserForm() {
@@ -187,32 +186,32 @@ function setCareerInfo(info) {
     if(info.tutor){
         if(info.profileList.length != 0) {
             $("#careerProfile").append('<h4>Profile</h4>');
-            info.profileList.forEach(i => $("#careerProfile").append('<p>' + i + '</p>'));
+            info.profileList.forEach(i => $("#careerProfile").append('<span>' + i + '</span><br/>'));
             $("#careerProfile").append('<br/>');
         }
 
         if(info.awardsList.length != 0) {
             $("#careerAwards").append('<h4>Awards</h4>');
-            info.awardsList.forEach(i => $("#careerAwards").append('<p>' + i + '</p>'));
+            info.awardsList.forEach(i => $("#careerAwards").append('<span>' + i + '</span><br/>'));
             $("#careerAwards").append('<br/>');
         }
 
         if(info.workshopList.length != 0) {
             $("#careerWorkshop").append('<h4>Workshop</h4>');
-            info.workshopList.forEach(i => $("#careerWorkshop").append('<p>' + i + '</p>'));
+            info.workshopList.forEach(i => $("#careerWorkshop").append('<span>' + i + '</span><br/>'));
             $("#careerWorkshop").append('<br/>');
         }
 
         if(info.performanceList.length != 0) {
             $("#careerPerformance").append('<h4>Performance</h4>');
-            info.performanceList.forEach(i => $("#careerPerformance").append('<p>' + i + '</p>'));
+            info.performanceList.forEach(i => $("#careerPerformance").append('<span>' + i + '</span><br/>'));
             $("#careerPerformance").append('<br/>');
         }
     }
 
     if(info.classList.length != 0) {
         $("#careerClass").append('<h4>Class</h4>');
-        info.classList.forEach(i => $("#careerClass").append('<p>' + i + '</p>'));
+        info.classList.forEach(i => $("#careerClass").append('<span>' + i + '</span><br/>'));
         $("#careerClass").append('<br/>');
     }
 }
